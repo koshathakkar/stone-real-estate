899941a49e09aeea76192b295f20b8e1845cb2c3
koshathakkar <koshathakkar2003@gmail.com>
2024-02-09 11:36:49 +0530

    2nd commmit

--- js/main.js
+++ js/main.js
@@ -1990,3 +1990,26 @@
 
   
 })(jQuery);
+
+// ===== NAVBAR ===== //
+$(window).scroll(function() {
+  
+    let position    =   $(this).scrollTop();
+      if (position >= 100) {
+        $('.nav-menu').addClass('costum-navbar');
+      } else {
+        $('.nav-menu').removeClass('costum-navbar');
+      }
+    
+  });
+  
+  
+  // ===== HAMBURGER BUTTON ===== //
+  $(document).ready(function() {
+    
+    $('.nav-button').click(function() {
+      $('.nav-button').toggleClass('change');
+    })
+      
+  });
+  
